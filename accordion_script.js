6f8743d7f4667408f1debcd2b09e24292254db97
Sayan Banerjee <sayanbanerjee82@gmail.com>
2018-09-13 00:33:43 +0530

    Delete accordion_script.js

--- accordion_script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-	$(".heading").click(function(){
-		let nxt=$(this).next().css("display")
-		if(nxt==="none")
-			$(this).next().css("display","block")
-		else
-			$(this).next().css("display","none")
-	});		
-});
